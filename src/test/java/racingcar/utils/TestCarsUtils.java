e5ea544fda04caefeef92c487f6b7216d3f1270b
sangho lee <sangholee.dev@gmail.com>
2022-04-25 21:41:03 +0900

    feat: 자동차 테스트용 util 추가

---
+++ src/test/java/racingcar/utils/TestCarsUtils.java
@@ -0,0 +1,38 @@
+package racingcar.utils;
+
+import static java.lang.String.join;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.car.Car;
+import racingcar.car.CarName;
+import racingcar.game.InputCarNames;
+
+public class TestCarsUtils {
+
+    public static final String DELIMITER = ", ";
+
+    public static List<Car> testCarList() {
+        List<Car> carList = new ArrayList<>();
+
+        for (int i = 0; i < Randoms.pickNumberInRange(1, 10); i++) {
+            carList.add(Car.of(randomCarName()));
+        }
+
+        return carList;
+    }
+
+    public static InputCarNames testInputCarNames() {
+        List<String> carStringList = new ArrayList<>();
+        for (int i = 0; i < Randoms.pickNumberInRange(1, 10); i++) {
+            carStringList.add(randomCarName().getName());
+        }
+
+        return InputCarNames.valueOf(join(DELIMITER, carStringList));
+    }
+
+    private static CarName randomCarName() {
+        return CarName.of("car_" + Randoms.pickNumberInRange(1, 9));
+    }
+}
